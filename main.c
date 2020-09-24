0fad89c404e7f9451cee743bd7a2b5c93d0d243d
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-09-24 16:16:26 +0900

    Practice3_Ver.1
    변수 2개 (min,sec) 사용

--- main.c
+++ main.c
@@ -4,15 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a,b;
-	 printf("input two integers :");
-	 scanf("%i %i", &a, &b);
-	 
-	 printf("\n + result is %i", a+b);
-	 printf("\n - result is %i", a-b);
-	 printf("\n * result is %i", a*b);
-	 printf("\n / result is %i", a/b);
-	 printf("\n %% result is %i", a%b);
+	int min,sec;
+	printf("input the second :");
+	scanf("%i", &sec);
+	
+	min = sec/60;
+	
+	printf("the time is %i : %i",min,sec%60);
 	
 	return 0;
 }
