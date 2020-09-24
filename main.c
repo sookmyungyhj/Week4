370f5fe95ce7cdf8262c960bcc9635025df378d6
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-09-24 16:29:05 +0900

    Practice4
    변수 2개(year,value) 이용
    윤년계산
    연도가 4로 나누어 떨어지지만 100으로 나누어 떨어지지 않는 경우와 400으로 나누어 떨어지는 경우

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sec;
-	printf("input the second :");
-	scanf("%i", &sec);
+	int year,value;
+	printf("input the year :");
+	scanf("%i", &year);
 	
-	printf("the time is %i : %i",sec/60,sec%60);
+	value = year%4==0 && year%100!=0 || year%400==0;
+	
+	printf("is the year %i the leap year? : %i",year,value);
 	
 	return 0;
 }
